60bc92d598a55a21fb9415d0fcb5ca4a5dafebc3
Morteza <mormoroth@gmail.com>
2019-08-13 13:32:37 +0200

    FIX Backup deletion

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ export PASSWORD=PWrootPassWordPW
 export DATE=$(date +%Y-%m-%d-%H-%M)
 export BACKUP_DIR="/root/mysql-backups/backups/"
 mkdir -p $WORK_DIR $BACKUP_DIR
-ls -t $BACKUP_DIR | tail -n +2 | xargs rm --
+cd $BACKUP_DIR && ls -t $BACKUP_DIR | tail -n +2 | xargs rm --
 innobackupex --user=$USERNAME --password=$PASSWORD --no-timestamp --stream=tar $BACKUP_DIR > ${BACKUP_DIR}${DATE}.tar.gz 2> ${WORK_DIR}/log.txt
 if tail -1 "${WORK_DIR}/log.txt" | grep -i "completed OK"; then
     printf "Backup successful!\n"
